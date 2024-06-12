076648cbf4e5558d03bd47f82fb349dd85de4bb2
lmoranallen <lmoranallen@gmail.com>
2024-06-12 21:08:36 +0100

    Separated initialisation functionality. Linked Player Spaceship Sprite class to initialisation flow

--- game_screen/screen.py
+++ game_screen/screen.py
@@ -1,41 +1,72 @@
-from pygame import display, Vector2, draw, font
+from pygame import display, Vector2, draw, font, time, quit, QUIT, event
+
+from sprites.player import SpaceShip
 
 SCREEN_WIDTH = 720
 SCREEN_HEIGHT = 1280
 BACKGROUND_COLOR = (68, 0, 19) # dark burgundy
 TITLE = "Challenge #65: Space Invaders"
+CLOCK = time.Clock()
+PLAYING = True
+
+screen = display.set_mode([SCREEN_HEIGHT, SCREEN_WIDTH])
 
 class GameScreen():
-    def __init__(self):
-        super().__init__()
-        font.init()
+    
+    def initialiseGame(self, player):
         FONT = font.SysFont("Arial", 16)
-        screen = display.set_mode([SCREEN_HEIGHT, SCREEN_WIDTH])
 
-        
+        # create the starting assets
         screen.fill(BACKGROUND_COLOR)
-        initial_pos = Vector2(screen.get_width()/2, screen.get_height()-60)
-        draw.circle(screen, "orange", initial_pos, 20)
 
-        score_text = FONT.render("SCORE",True, (255,255,255))
+        # initial spaceship position
+        draw.circle(screen, "orange", player.position, 20)
+
+        # SCORE font
+        score_text = FONT.render(f"SCORE: {player.score}",True, (255,255,255))
         screen.blit(score_text, (220,20))
 
-        lives_text = FONT.render("LIVES",True, (255,255,255))
+        # LIVES font
+        lives_text = FONT.render(f"LIVES: {player.lives}",True, (255,255,255))
         screen.blit(lives_text, (820,20))
 
-        
-
+        #draw line and spaceship cover
         draw.line(screen, (0,255,0), [0.1*screen.get_width(), screen.get_height()-10], [0.9*screen.get_width(), screen.get_height()-10], 3)
         for i in range(0,4):
             draw.rect(screen, (0,255,0), [0.2*(i+1)*screen.get_width()-40, screen.get_height()-240, 75, 60])
 
+        #draw enemies
         for j in range(0,3):
             for k in range(0,6):
                 draw.rect(screen, (76,12,250), [0.14*(k+1)*screen.get_width(), 0.1*j*screen.get_height()+100, 15, 15])
 
 
         display.set_caption(TITLE)
-        display.flip()
+
+    def __init__(self):
+        super().__init__()
+        font.init()
+        PLAYING = True
+        player = SpaceShip("orange", screen)
+        self.initialiseGame(player)
+
+        
+
+        print("YOUR CHARACTER", player.lives, player.position)
+
+        while PLAYING:
+
+            # pygame.QUIT event means the user clicked X to close your window
+            for event_elem in event.get():
+                if event_elem.type == QUIT:
+                    PLAYING = False
+
+            
+
+            display.flip()
+            dt = CLOCK.tick(60)/1000
+        quit()
+
         
     def getDetails(self):
         return display.Info()
--- sprites/player.py
+++ sprites/player.py
@@ -1,4 +1,4 @@
-from pygame import Surface, Vector2
+from pygame import Surface, Vector2, key, K_LEFT, K_RIGHT
 from pygame.sprite import Sprite
 
 SHIP_HEIGHT = 25
@@ -6,14 +6,34 @@ SHIP_WIDTH = 20
 
 
 class SpaceShip(Sprite):
-    def __init__(self, color):
+    def __init__(self, color, screen):
         Sprite.__init__(self)
 
         self.image = Surface([SHIP_WIDTH, SHIP_HEIGHT])
         self.image.fill(color)
+        self.lives = 3
+        self.score = 0
+        self.position = Vector2(screen.get_width()/2, screen.get_height()-60)
 
         self.rect = self.image.get_rect()
     
     def draw(self, surface):
         surface.blit(self.image, self.rect)
+
+    def getLives(self):
+        return self.lives
+    
+    def moveSpaceship(self, screen, dt):
+        keys_pressed = key.get_pressed()
+
+        if (keys_pressed[K_LEFT] and self.position.x > 0.1*screen.get_width()):
+            self.position.x -= 10
+
+        if (keys_pressed[K_RIGHT] and self.position.x < 0.9*screen.get_width()):
+            self.position.x += 10
+        
+
+
+
+
     
